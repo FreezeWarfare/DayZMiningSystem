2d72134abf7d5f2b5a20fe9822f49ad5829957a5
MrFreezeGamesPC <FreezeWarfare@gmail.com>
2024-09-06 22:55:37 +0100

    Update config.cpp

--- MrFreezeMiningTranslate/4_world/scripts/placement/config.cpp
+++ MrFreezeMiningTranslate/4_world/scripts/placement/config.cpp
@@ -1,12 +1,3 @@
-////////////////////////////////////////////////////////////////////
-//DeRap: 4_world\scripts\placement\config.bin
-//Produced from mikero's Dos Tools Dll version 9.10
-//https://mikero.bytex.digital/Downloads
-//'now' is Mon Jun 03 06:19:53 2024 : 'file' last modified on Sun Jun 02 01:06:37 2024
-////////////////////////////////////////////////////////////////////
-
-#define _ARMA_
-
 class CfgPatches
 {
 	class FreezeMining_Base_Kit
@@ -43,19 +34,19 @@ class CfgVehicles
 	class FreezeFurnace_Kit: FreezeMining_Kit_Base
 	{
 		scope = 2;
-		displayName = "#STR_FreezeFurnace_Kit";
-		descriptionShort = "#STR_FreezeFurnace_Kit_desc";
+		displayName = "$STR_FreezeFurnace_Kit";
+		descriptionShort = "$STR_FreezeFurnace_Kit_desc";
 	};
 	class FreezeAnvil_Kit: FreezeMining_Kit_Base
 	{
 		scope = 2;
-		displayName = "#STR_FreezeAnvil_Kit";
-		descriptionShort = "#STR_FreezeAnvil_Kit_desc";
+		displayName = "$STR_FreezeAnvil_Kit";
+		descriptionShort = "$STR_FreezeAnvil_Kit_desc";
 	};
 	class FreezeGrindingStone_Kit: FreezeMining_Kit_Base
 	{
 		scope = 2;
-		displayName = "#STR_FreezeGrindingStone_Kit";
-		descriptionShort = "#STR_FreezeGrindingStone_Kit_desc";
+		displayName = "$STR_FreezeGrindingStone_Kit";
+		descriptionShort = "$STR_FreezeGrindingStone_Kit_desc";
 	};
 };
